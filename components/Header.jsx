17fa9cbd9072b5f6f934b0587b9fa7a0781987a6
root <root@LAPTOP-P07JSRR3.localdomain>
2022-07-30 11:18:05 +0530

    hardhat way

---
+++ components/Header.jsx
@@ -0,0 +1,32 @@
+import { useMoralis } from "react-moralis"
+import { useEffect } from "react"
+export default function Header() {
+    const { enableWeb3, account, isWeb3Enabled, Moralis, deactivateWeb3 } = useMoralis()
+    useEffect(() => {
+        if (isWeb3Enabled) return
+        if (typeof window !== "undefined") {
+            if (window.localStorage.getItem("connected")) {
+                enableWeb3()
+            }
+        }
+    }, [isWeb3Enabled])
+    useEffect(() => {
+        Moralis.onAccountChanged(() => {
+            console.log(`Account changed to ${account}`)
+            if (account == null) {
+                window.localStorage.removeItem("connected")
+                deactivateWeb3()
+                console.log("null account found")
+            }
+        })
+    }, [])
+    return (<div>
+        {account ? (<div>Connected to <br /> {account.slice(0, 6)}...{account.slice(account.length - 4)}</div>) : (<button onClick={async () => {
+            await enableWeb3()
+            if (typeof window !== "undefined") {
+                window.localStorage.setItem("connected", "injected")
+            }
+        }}>Connect</button>)}
+
+    </div >)
+}
---
+++ pages/index.js
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+import Header from '../components/Header'
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Lottery</title>
+
+      </Head>
+      <Header></Header>
+
+    </div >
+  )
+}
